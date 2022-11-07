98c076081ec52caf96f4a2a822bb86ac18386d89
Dmitriy Yegorov <yegorovd14@gmail.com>
2022-11-07 12:21:23 +0000

    refactor code

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
 const board = document.querySelector("#board");
 const colors = ['#3b89e7', '#00adf7', '#00cbe4', '#00e2b8', '#92f28a', '#f9f871', '#846dc0', '#9a5491'];
-const SQUARES_NUMBER = 1500;
+const SQUARES_NUMBER = 2000;
 
 for(let i = 0; i < SQUARES_NUMBER; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
 
     square.addEventListener("mouseover", () => {
-        setColor(square)
+        setColor(square, getColors())
     });
     square.addEventListener("mouseleave", () => {
         removeColor(square);
     });
-    
 
     board.append(square);
 }
 
-function setColor(e) {
-    const color = getColors();
+function setColor(e, color) {
+    // REFACTOR(22 -> 10)
+    // const color = getColors();
+
     e.style.background = color;
     e.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
 }
@@ -29,6 +30,9 @@ function removeColor(e) {
 }
 
 function getColors() {
-   const index = Math.floor(Math.random() * colors.length)
-   return colors[index];
+    return colors[Math.floor(Math.random() * colors.length)];
+
+    // REFACTOR(35-36 -> 33)
+//    const index = Math.floor(Math.random() * colors.length)
+//    return colors[index];
 }
